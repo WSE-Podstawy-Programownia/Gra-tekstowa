2ad1e4151e229085bae57bcb9a92dbcac2ae4fc3
Paulina <madameleraye@gmail.com>
2020-07-03 15:03:39 +0200

    test

--- Gra tekstowa.cs
+++ Gra tekstowa.cs
@@ -21,6 +21,7 @@ namespace Game
                 Console.WriteLine("1.Game");
                 Console.WriteLine("2.Instruction");
                 Console.WriteLine("3.Exit");
+                Console.WriteLine(" ");
 
                 choice = Console.ReadLine();
 
